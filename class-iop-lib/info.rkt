8cc2955723c83d56f1df48218ce6a32c2c6fbd7f
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-28 16:10:09 -0500

    Add info.rkt and license to class-iop-lib.

---
+++ class-iop-lib/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection 'multi)
+(define version "1.0")
+(define deps '(("base" #:version "6.2.900.6")))
+(define pkg-authors '(ryanc))
+
+(define test-responsibles '((all ryanc)))
