a4fd4beea6d9c8786a108ac12c997097b90191a9
Vincent St-Amour <stamourv@racket-lang.org>
2015-08-13 14:10:53 -0500

    Add missing dep.

--- class-iop-doc/info.rkt
+++ class-iop-doc/info.rkt
@@ -4,5 +4,6 @@
 (define version "1.0")
 (define deps '(("base" #:version "6.2.900.6")
                "class-iop-lib"))
-(define build-deps '(("scribble-lib")))
+(define build-deps '("scribble-lib"
+                     "racket-doc"))
 (define pkg-authors '(ryanc))
